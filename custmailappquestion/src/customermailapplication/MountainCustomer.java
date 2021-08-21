abe937c2225d7f014a277a66b1a66bc77cb24f62
sugarDOTx3 <86604764+sugarDOTx3@users.noreply.github.com>
2021-08-21 19:28:45 +0700

    Update MountainCustomer.java

--- custmailappquestion/src/customermailapplication/MountainCustomer.java
+++ custmailappquestion/src/customermailapplication/MountainCustomer.java
@@ -13,7 +13,6 @@ package customermailapplication;
 //make this class a subclass of customer
 public class MountainCustomer extends Customer {
 
-    //Complete this class
         @Override
     public String createMail() {
         return "Mountain Customer";
