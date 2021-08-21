717a272cd2b45742240339d14e18fd572e5fc818
sugarDOTx3 <86604764+sugarDOTx3@users.noreply.github.com>
2021-08-21 19:28:33 +0700

    Update DelinquentCustomer.java

--- custmailappquestion/src/customermailapplication/DelinquentCustomer.java
+++ custmailappquestion/src/customermailapplication/DelinquentCustomer.java
@@ -12,9 +12,8 @@ package customermailapplication;
  */
 //make this class a subclass of customer
 public class DelinquentCustomer extends Customer{
-    
-    //Complete this class
-            @Override
+   
+    @Override
     public String createMail() {
         return "Delinquent Customer";
     }
